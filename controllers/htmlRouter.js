eca39c5b371a7491a9ff4a048c2aa10df0756e28
Jimmy Chemuku <jimmycn03@gmail.com>
2025-02-24 13:06:41 +0300

    feat(routing): HTML router
    Added a module that will serve HTML pages once a specific route is visited. Currently serves the main menu

---
+++ controllers/htmlRouter.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const htmlRoute = express.Router();
+const template = require("../templates/template");
+
+/**
+ * The main route
+ */
+htmlRoute.get("/", (req, res) => {
+  res.sendFile(template("main-menu.html"));
+});
+
+module.exports = htmlRoute;
