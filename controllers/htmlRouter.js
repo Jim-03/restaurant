50c83339c389a9c225a3719400366df11ac05359
Jimmy Chemuku <jimmycn03@gmail.com>
2025-03-07 11:38:25 +0300

    style: Used semistandard to style the module to match JavaScript's coding standards

--- controllers/htmlRouter.js
+++ controllers/htmlRouter.js
@@ -1,12 +1,12 @@
-const express = require("express");
+const express = require('express');
 const htmlRoute = express.Router();
-const template = require("../templates/template");
+const template = require('../templates/template');
 
 /**
  * The main route
  */
-htmlRoute.get("/", (req, res) => {
-  res.sendFile(template("main-menu.html"));
+htmlRoute.get('/', (req, res) => {
+  res.sendFile(template('main-menu.html'));
 });
 
 module.exports = htmlRoute;
