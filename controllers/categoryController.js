e0cb08ca021ae6162b011b2bb69e13219173f802
Jimmy Chemuku <jimmycn03@gmail.com>
2025-03-08 13:46:03 +0300

    feat: Category controller
    A controller module for the category model that handles incoming requests and outgoing responses between the client and server

---
+++ controllers/categoryController.js
@@ -0,0 +1,32 @@
+const service = require('../services/categoryService');
+const express = require('express');
+const categoryApi = express.Router();
+const getStatusCode = require('./getStatusCode');
+
+categoryApi.get('/api/category', (req, res) => {
+  service.getAll()
+    .then(response => {
+      res.status(getStatusCode(response.status)).json(response);
+    });
+});
+
+categoryApi.post('/api/category', (req, res) => {
+  service.addCategory(req.body)
+    .then(response => {
+      res.status(getStatusCode(response.status)).json(response);
+    });
+});
+
+categoryApi.put('/api/category/:id', (req, res) => {
+  service.updateCategory(Number(req.params.id), req.body)
+    .then(response => {
+      res.status(getStatusCode(response.status)).json(response);
+    });
+});
+
+categoryApi.delete('/api/category/:id', (req, res) => {
+  service.deleteCategory(Number(req.params.id))
+    .then(response => {
+      res.status(getStatusCode(response.status)).json(response);
+    });
+});
