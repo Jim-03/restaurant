0a259fbb4e15a4439a76bcba557daec0fd47c117
Jimmy Chemuku <jimmycn03@gmail.com>
2025-03-23 20:01:15 +0300

    style: styled using semistandard

--- repositories/orderRepository.js
+++ repositories/orderRepository.js
@@ -56,7 +56,7 @@ async function findByServer (id) {
 
 /**
  * Retrieves a list of orders made  between two dates
- * @param {Date} start 
+ * @param {Date} start
  * @param {Date} end
  * @returns {Promise<orderModel[] | null>} An object containing the list of orders or null
  * @throws {Error} In case of any errors
